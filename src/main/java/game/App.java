2706a74835595230e24fd70ca5f2b1ca6aafe895
[Laurel] <[le.nh4026@gmail.com]>
2025-09-24 07:28:53 +0700

    Refactor Renderer construction

--- src/main/java/game/App.java
+++ src/main/java/game/App.java
@@ -4,6 +4,7 @@ import game.Renderering.Renderer;
 import javafx.application.Application;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
@@ -13,13 +14,13 @@ public class App extends Application {
     @Override
     public void start(Stage stage) { //Stage = cửa sổ
         Rectangle2D screenBounds = Screen.getPrimary().getBounds();
-        //Các renderer có canvas khác nhau (ở 3 tầng khác nhau trong stackPane)
-        Renderer backgroundRenderer = new Renderer(screenBounds);
-        Renderer entityRenderer = new Renderer(screenBounds);
-        Renderer uiRenderer = new Renderer(screenBounds);
+        //Canvas khác nhau (ở 3 tầng khác nhau trong stackPane)
+        Canvas backgroundCanvas = new Canvas(screenBounds.getWidth(), screenBounds.getHeight());
+        Canvas entityCanvas = new Canvas(screenBounds.getWidth(), screenBounds.getHeight());
+        Canvas uiCanvas = new Canvas(screenBounds.getWidth(), screenBounds.getHeight());
         StackPane root = new StackPane();
         //StackPane  định nghĩa thứ tự render các Canvas trong mainScene 
-        root.getChildren().addAll(backgroundRenderer.getCanvas(), entityRenderer.getCanvas(), uiRenderer.getCanvas());
+        root.getChildren().addAll(backgroundCanvas, entityCanvas, uiCanvas);
 
         stage.setX(screenBounds.getMinX());
         stage.setY(screenBounds.getMinY());
@@ -30,9 +31,8 @@ public class App extends Application {
         stage.setScene(mainScene);
         stage.setTitle("Arkanoid");
         stage.show();
-
         //GameEngine quản lí logic game
-        GameEngine engine = GameEngine.getInstance(entityRenderer);
+        GameEngine engine = GameEngine.getInstance(backgroundCanvas, entityCanvas, uiCanvas);
         engine.run();
     }
     
