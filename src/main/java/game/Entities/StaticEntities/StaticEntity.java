eafa4300b43726a2baf2f197f07e83186d42c74c
[Laurel] <[le.nh4026@gmail.com]>
2025-10-07 12:19:02 +0700

    refactor (huy): sửa StaticEntity có thể khởi tạo những Entity di chuyển có giới hạn theo 1 pattern định trước

--- src/main/java/game/Entities/StaticEntities/StaticEntity.java
+++ src/main/java/game/Entities/StaticEntities/StaticEntity.java
@@ -3,7 +3,33 @@ package game.Entities.StaticEntities;
 import game.Entities.Entity;
 
 public abstract class StaticEntity extends Entity {
+    public static final int LEFT = -1;
+    public static final int RIGHT = 1;
+    private int direction;
+    private double vel; 
+    private double movementLeftBound, movementRightBound, movementUpperBound, movementLowerBound;  
     public StaticEntity(double x, double y, double w, double h) {
         super(x, y, w, h);
+    }
+
+    public StaticEntity(double x, double y, double w, double h, int initialDirection, double vel, double movementLeftBound, double movementRightBound, double movementUpperBound, double movementLowerBound) {
+        super(x, y, w, h);
+        this.direction = initialDirection;
+        this.vel = vel;
+        this.movementLeftBound = movementLeftBound;
+        this.movementRightBound = movementRightBound;
+        this.movementUpperBound = movementUpperBound;
+        this.movementLowerBound = movementLowerBound;
+    }
+
+    public void horizontalOscillate() {
+        setX(getX() + vel * direction);
+        if (getX() > movementRightBound) {
+            setX(movementRightBound); //adjust to bound
+            direction = LEFT;
+        } else if (getX() < movementLeftBound) {
+            setX(movementLeftBound); //adjust to bound
+            direction = RIGHT;
+        }
     }
 }
